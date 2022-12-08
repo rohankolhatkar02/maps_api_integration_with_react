2b831893a160538ee6874676a9c0dd2945d96a3b
Rohan Kolhatkar <rohankolhatkar02@gmail.com>
2022-12-09 00:33:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react'
+import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+
+import{useJsApiLoader,GoogleMap,Marker,Autocomplete,DirectionsRenderer} from '@react-google-maps/api'
+import { width } from '@mui/system'
+import { useRef, useState } from 'react'
+
+const center = { lat:48.8584 ,lng: 2.2945}
 
 function App() {
+
+  const {isLoaded} = useJsApiLoader({
+    googleMapsApiKey:process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries:['places'],
+  })
+
+  const [map,setMap] = useState( /**@type google.maps.Map */(null))
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
+  
+  /**@type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef()
+/**@type React.MutableRefObject<HTMLInputElement> */
+  const destinationRef = useRef()
+
+  if(!isLoaded){
+    return <SkeletonText />
+  }
+
+  async function calculateRoute() {
+    if(originRef.current.value ===''||destinationRef.current.value ===''){
+      return
+    }
+    const directionService = new google.maps.DirectionsService()
+    const results = await directionService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      travelMode: google.maps.TravelMode.DRIVING
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+  }
+
+  function clearRoute(){
+    setDirectionsResponse(null)
+    setDistance('')
+    setDuration('')
+    originRef.current.value = ''
+    destinationRef.current.value = ''
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Flex
+      position='relative'
+      flexDirection='column'
+      alignItems='center'
+      bgColor='blue.200'
+      h='100vh'
+      w='100vw'
+    >
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+            {/*google map box*/}
+            <GoogleMap
+             center={center} 
+             zoom={15} 
+             mapContainerStyle={{width:'100%',height:'100%'}}
+             options={{
+              zoomControl:false,
+              streetViewControl:false,
+              mapTypeControl:true,
+              fullscreenControl:false
+             }}
+             onLoad={map =>setMap(map)} 
+             >
+              <Marker position={center}/>
+              {directionsResponse && (
+              <DirectionsRenderer directions={directionsResponse}/>
+            )}
+            </GoogleMap>
+      </Box>
+      <Box
+        p={4}
+        borderRadius='lg'
+        mt={4}
+        bgColor='white'
+        shadow='base'
+        minW='container.md'
+        zIndex='modal'
+      >
+        <HStack spacing={4}>
+          <Autocomplete>
+          <Input type='text' placeholder='Origin' ref={originRef}/>
+          </Autocomplete>
+          
+          <Autocomplete>
+          <Input type='text' placeholder='Destination' ref={destinationRef}/>
+          </Autocomplete>
+          <ButtonGroup>
+            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label='center back'
+              icon={<FaTimes />}
+              onClick={clearRoute}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent='space-between'>
+          <Text>Distance: {distance} </Text>
+          <Text>Duration:{duration} </Text>
+          <IconButton
+            aria-label='center back'
+            icon={<FaLocationArrow />}
+            isRound
+            onClick={() => map.panTo(center)}
+          />
+        </HStack>
+      </Box>
+    </Flex>
+  )
 }
 
-export default App;
+export default App
